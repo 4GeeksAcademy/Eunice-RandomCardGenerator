85c11b00c17b19fa86a5e3937b118a472957b0a2
eunice_sin_h <71563392+EuniceHG@users.noreply.github.com>
2023-06-28 20:16:03 +0000

    addingCode

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  generateRandomCard();
 };
+
+function generateRandomCard() {
+  let suits = ["heart", "spade", "club", "diamond"];
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "King",
+    "Queen",
+    "Jack",
+    "Ace"
+  ];
+
+  // Generate random indices for suit and value arrays
+  let suitIndex = Math.floor(Math.random() * suits.length);
+  let valueIndex = Math.floor(Math.random() * values.length);
+
+  // Get the card element
+  let card = document.querySelector(".card");
+
+  // Remove existing suit class
+  card.classList.remove("heart", "spade", "club", "diamond");
+
+  // Add the new suit class based on the random index
+  card.classList.add(suits[suitIndex]);
+
+  // Set the card value based on the random index
+  document.querySelector(".number").textContent = values[valueIndex];
+}
