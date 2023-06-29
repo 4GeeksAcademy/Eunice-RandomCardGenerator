3e9568337f87c7982be10f6f851878bf3b16cc33
eunice_sin_h <71563392+EuniceHG@users.noreply.github.com>
2023-06-29 00:08:50 +0000

    addingCodee

--- src/app.js
+++ src/app.js
@@ -29,45 +29,42 @@ function generateRandomCard() {
   let suitSymbol;
   let suitColor;
 
-  // Generate random indices for suit and value arrays
+  // Random indices for suit and value arrays
   let suitIndex = Math.floor(Math.random() * suits.length);
   let valueIndex = Math.floor(Math.random() * values.length);
 
-  // Get the card element
+  // Card element
   let card = document.querySelector(".card");
 
   // Remove existing suit class
   card.classList.remove("heart", "spade", "club", "diamond");
 
-  // Add the new suit class based on the random index
+  // New suit class based on the random index
   card.classList.add(suits[suitIndex]);
 
   switch (suits[suitIndex]) {
     case "heart":
       suitSymbol = "&hearts;";
-      suitColor = getRandomColor(["black", "red"]);
       break;
     case "spade":
       suitSymbol = "&spades;";
-      suitColor = getRandomColor(["black", "red"]);
       break;
     case "club":
       suitSymbol = "&clubs;";
-      suitColor = getRandomColor(["black", "red"]);
       break;
     case "diamond":
       suitSymbol = "&diams;";
-      suitColor = getRandomColor(["black", "red"]);
       break;
   }
 
-  // Set the suit symbol for the card
+  // Suit symbol and color for the card
+  suitColor = getRandomColor(["black", "red"]);
   document.querySelectorAll(".suits").forEach(suit => {
     suit.innerHTML = suitSymbol;
     suit.style.color = suitColor;
   });
 
-  // Set the card value color based on the random index
+  // Card value and color based on the random index
   document.querySelector(".number").textContent = values[valueIndex];
   document.querySelector(".number").style.color = suitColor;
 }
